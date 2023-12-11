9d599a637902095546691ae1b8fa27863941aa91
João Pedro <joaopedroalexandrino6@hotmail.com>
2023-12-11 10:30:23 -0300

    Starting new project

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import java.awt.GridBagLayout;
+public class Main {
+    public static void main(String[] args) {
+        //Component JFrame
+        JFrame myWindow = new JFrame("TicTacToe Game");
+        myWindow.setSize(800,800);
+        myWindow.setVisible(true);
+
+    }
+}
